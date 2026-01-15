9c28adcd0e56cf1a50d55a072a55d698d71fca3f
Yaguera <ya-gotre@hotmail.com>
2026-01-15 10:04:29 -0300

    Add Navbar component, constants and public folders

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import gsap from 'gsap';
+import Navbar from './components/Navbar';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='text-6xl text-indigo-300 flex-center'>
-        App
-    </div>
+   <main>
+    <Navbar />
+   </main>
   )
 }
 
